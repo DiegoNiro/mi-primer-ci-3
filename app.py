8d54175e7710bb91a59bb05956611e535578f4f8
DiegoNiro <202012357@urp.edu.pe>
2025-05-03 02:46:36 +0000

    Nueva funcionalidad: multiplicación de múltiples números

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ def resta(a, b):
 def multiplicacion(a, b):
     return a * b
 
+def multiplicacion(a, b):
+    return a * b
+
+
 def division(a, b):
     if b == 0:
         raise ValueError("No se puede dividir por cero")
